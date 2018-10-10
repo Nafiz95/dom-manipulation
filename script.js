83c753cfc15c1395014e5ac5b0c2f2f4e3766942
Nafiz Sadman <nafizsadman420@gmail.com>
2018-10-10 03:12:22 +0000

    DOM EX 2a

--- script.js
+++ script.js
@@ -4,7 +4,14 @@ console.log(heading.innerText);
 heading.innerHTML="<h3>CHANGED IT</h3>"
 
 /* Task 2 using plain JavaScript */
-
+var textBtn=document.getElementById("addText2");
+textBtn.addEventListener("click",function(event)
+{
+    var task2=document.getElementById("task2a");
+    var para=document.createElement('p');
+    task2.innerHTML="<p>Hello World</p>";
+    task2.appendChild(para);
+});
 
 
 /* Task 4 using jQuery */
